4f599efe3fb340946c01dd6a04385fd42a6da548
Khanh Duong <verykhanhny@gmail.com>
2024-04-11 00:52:33 -0600

    remove cors

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,11 @@
 // Importing required modules
 const express = require('express');
-const cors = require('cors');
 
 // Create an Express application
 const app = express();
 
-// Enable CORS
-app.use(cors());
-
 // Define a route handler for a custom endpoint '/api/data'
 app.get('/api', (req, res) => {
-    console.log("reached")
     const data = {
         message: 'This is some data from the server!',
         timestamp: new Date().toISOString()
